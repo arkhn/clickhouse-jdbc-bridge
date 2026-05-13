bb426226d1e8381542761037851364bcc41c5abe
Olivier Girardot <olivier.girardot@arkhn.com>
2026-05-13 12:42:31 +0200

    fix: improve timeout and relax tls security defaults

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -23,11 +23,15 @@ start_server() {
 	if [ "$(echo ${CUSTOM_DRIVER_LOADER:="true"} | tr '[:upper:]' '[:lower:]')" != "true" ]; then
 		local classpath="./clickhouse-jdbc-bridge-shaded.jar:$(echo $(ls ${DRIVER_DIR:="drivers"}/*.jar) | tr ' ' ':'):."
 		java -XX:+UseContainerSupport -XX:+ExitOnOutOfMemoryError \
-			-Djava.util.logging.config.file=$JDBC_BRIDGE_HOME/logging.properties -Dnashorn.args=--language=es6 \
+			-Djava.util.logging.config.file=$JDBC_BRIDGE_HOME/logging.properties \
+			-Djava.security.properties=$JDBC_BRIDGE_HOME/java.security.override \
+			-Dnashorn.args=--language=es6 \
 			${JDBC_BRIDGE_JVM_OPTS:=""} -cp $classpath com.clickhouse.jdbcbridge.JdbcBridgeVerticle 2>&1 | grep --line-buffered -vE "(sun\.misc\.Unsafe|WARNING: Please consider reporting this to the maintainers|WARNING: A terminally deprecated method)"
 	else
 		java -XX:+UseContainerSupport -XX:+ExitOnOutOfMemoryError \
-			-Djava.util.logging.config.file=$JDBC_BRIDGE_HOME/logging.properties -Dnashorn.args=--language=es6 \
+			-Djava.util.logging.config.file=$JDBC_BRIDGE_HOME/logging.properties \
+			-Djava.security.properties=$JDBC_BRIDGE_HOME/java.security.override \
+			-Dnashorn.args=--language=es6 \
 			${JDBC_BRIDGE_JVM_OPTS:=""} -jar clickhouse-jdbc-bridge-shaded.jar 2>&1 | grep --line-buffered -vE "(sun\.misc\.Unsafe|WARNING: Please consider reporting this to the maintainers|WARNING: A terminally deprecated method)"
 	fi
 }
